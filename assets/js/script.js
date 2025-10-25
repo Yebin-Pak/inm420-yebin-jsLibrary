23006c4954043aa3698585534a908520039cf306
Yebin-Pak <dpqls64@naver.com>
2025-10-25 03:17:26 -0400

    102525 03:17

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// glide.js
+new Glide('.glide').mount()
+
+// lightbox2.js
+lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true
+})
+
+// AOS.js
+AOS.init();
+
+// parsley.js
+$('#form').parsley();
